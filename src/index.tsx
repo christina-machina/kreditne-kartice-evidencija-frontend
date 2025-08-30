b22d3fa036709fe19c0e0094b57eed2a92eff42b
Kristina Vidakovic <kristina.vidakovic@kodilion.hr>
2025-08-30 12:10:40 +0200

    instaliran axios, primeflex, primereact, react-hook-form i yup

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// PrimeReact CSS
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
